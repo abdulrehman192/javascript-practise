e64b2b14a38611e9670303b3297616df8618e4f0
abdulrehman192 <mr.abdulrehman.ar@gmail.com>
2021-06-08 12:57:58 +0500

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,28 @@
+//Variables in javascript
+//Strings in javascript
+let name = 'Ali Ahmad'; //variable declaration and initialization
+console.log(name); //usage of variable
+name = 'Zain'
+console.log(name);
+let fullname; //variable declaration with undefined status
+fullname = 'Ali Hamza';
+console.log(fullname);
+var cnic = "35305-7896321-6";
+console.log(cnic);
+
+//Numbers in javascript
+const age = 25;
+console.log(age);
+var x = 4545454848;
+var y = 545454;
+var check = false;
+console.log(check);
+
+check = (x > y);
+console.log(check);
+console.log(x / 0);
+var student_list = ['Ali', 'Zain', 'Ahamd', 'Zoya', 'komal', 'Hassan', 'Abdullah'];
+console.log(student_list);
+console.log(student_list[3]);
+var student_age = [20, 21, 19, 22, 25, 23];
+console.log('Name : ' + student_list[0] + '  Age : ' + student_age[0]);
